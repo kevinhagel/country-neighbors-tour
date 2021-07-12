b09ebbc97102b3825bd9d5d9e78aeafaf2303b89
JE72819 <kevinmichael.hagel@kbc.be>
2021-07-12 13:27:42 +0300

    Initial Commit

---
+++ src/main/java/com/vmw/cntour/CountryNeighborsTourApplication.java
@@ -0,0 +1,13 @@
+package com.vmw.cntour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryNeighborsTourApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CountryNeighborsTourApplication.class, args);
+  }
+
+}
